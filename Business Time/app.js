d6b5af1a482337266bb152bfc24263842f83bcb0
Kevin Clark <naeaman@gmail.com>
2019-09-03 09:13:37 -0600

    Sept 3 push

---
+++ Business Time/app.js
@@ -0,0 +1,7 @@
+// const dateZ = document.getElementById("resDate").value; 
+// const resPush = document.getElementById(resButton);
+
+resButton.addEventListener("click" , (e) => {
+    e.preventDefault()
+    window.alert("Your dog's daycare date is set!")
+})
